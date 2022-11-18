42b815a414d790b9433659cb72d3a71d5e881cf1
BlakeBrooker <116875628+BlakeBrooker@users.noreply.github.com>
2022-11-18 11:01:13 +0000

    Add files via upload

--- js/sketch.js
+++ js/sketch.js
@@ -3,6 +3,7 @@ let r;
 let res = 362;
 let xoff = 0;
 
+
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
   angleMode(DEGREES);
@@ -14,14 +15,14 @@ function setup() {
 function draw() {
   background(0);
 	let offset = map(noise(xoff), 0, 1, 2, 5);
-  for(let i = 7; i < 50; i++){
+  for(let i = 9; i < 50; i++){
     stroke(i * angle, 100, 50);
     strokeWeight(3 * cos(i));
     rotateX(angle);
-    beginShape(LINES);
-    for(let a = 11; a < res; a+=res/90){
-        let r = random(60);
-        r = (50 * abs(sin(a * 10))) + 200;
+    beginShape(TRIANGLE_STRIP);
+    for(let a = 11; a < res; a+=res/180){
+        let r = random(180);
+        r = (mouseX * abs(sin(a * 10))) + 200;
       let x = r * cos(a * offset);
       let y = r * sin(a);
 			let z = r * cos(angle);
