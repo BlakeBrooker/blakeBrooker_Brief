a0c6be6e272387ef5a4ed7293153d8fc8a6d0cd6
BlakeBrooker <116875628+BlakeBrooker@users.noreply.github.com>
2022-11-29 11:56:44 +0000

    Add files via upload

--- js/sketch.js
+++ js/sketch.js
@@ -7,7 +7,8 @@ let xoff = 0;
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL); // how big the canvas is
   angleMode(DEGREES);
-  colorMode(HSB,360, 100, 100, 100); // what colour format to copy
+  colorMode(HSB); // what colour format to copy
+
   noFill();
   
 }
@@ -16,25 +17,29 @@ function draw() {
   background(0);
 	let offset = map(noise(xoff), 0, 1, 2, 5);
   for(let i = 3; i < 50; i++){ // draws the pattern up to 50 times and then restarts
-    stroke(i * angle, 100, 50);
-    strokeWeight(3 * cos(i));
+    stroke(i * angle, 180, 50);
+    strokeWeight(2 * cos(i));
     rotateY(angle); // rotates the Shape around the Y coord at the set angle
     beginShape(); // functions allow creating more complex forms. 
      // beginShape() begins recording vertices for a shape 
     for(let a = random(); a < res; a+=res/180){
-        let r = random(- 200, 180); //going to the far left of the canvas inverts the pattern, going to the right inverts it back to normal and zooms in
+        let r = random(- 200, 200); //going to the far left of the canvas inverts the pattern, going to the right inverts it back to normal and zooms in
         r = (mouseX * abs(sin(a * 10))) + 200; // mouseX variable moves the pattern along with the mouse moving on the x coord
       let x = r * cos(a * offset);
       let y = r * sin(a); // y = r times sin(and the A value)
 			let z = r * cos(angle); // z value = r value times cos(and angle value)
-           
+      vertex(10, 35);
+      vertex(90, 35);
+      vertex(10, 65);
+      
+      
 
       vertex(x, y, z); // specifies the vertex coords for points, lines, triangles etc
     }
     endShape(); //endShape() stops recording
   }
-  angle += 0.02;
-  r += 0.01;
+  angle += 0.01;
+  r += 0.04;
 }
 
 function doubleClicked() {
