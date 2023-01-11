ee2705fa8653e9f3fc7857abc11030027d05985d
Blake <116875628+BlakeBrooker@users.noreply.github.com>
2023-01-11 08:31:49 +0000

    Add files via upload

--- js/sketch.js
+++ js/sketch.js
@@ -1,4 +1,4 @@
-let angle = 9; // angle value = what angle the pattern moves at
+let angle = 90; // angle value = what angle the pattern moves at
 let r; // lets r = any value
 let res = 1000;
 let xoff = 0;
@@ -12,16 +12,20 @@ var colorPicker;
 
 
 
+
+
+
 function setup() {
-  createCanvas(windowWidth, windowHeight, WEBGL); // how big the canvas is
+  createCanvas(1000, 1000, WEBGL); // how big the canvas is
   
   angleMode(DEGREES);
   colorMode(RGB); // what colour format to copy
   colorPicker = createColorPicker('#ffff');  // starts off as white
 
-  colorPicker.position(50, height); // colour picker is in bottom left corner
+  colorPicker.position(30, height); // colour picker is in bottom left corner
   noFill();
-  
+
+
 }
 
 function draw() {
@@ -38,11 +42,13 @@ function draw() {
      // beginShape() begins recording vertices for a shape 
     for(let a = random(); a < res; a+=res/180){
         let r = random(- 200, 200); //going to the far left of the canvas inverts the pattern, going to the right inverts it back to normal and zooms in
-        r = (mouseX * abs(sin(a * 10))) + 200; // mouseX variable moves the pattern along with the mouse moving on the x coord
+        r = (50 * abs(sin(a * 10))) + 200; // mouseX variable moves the pattern along with the mouse moving on the x coord
       let x = r * cos(a * offset);
       let y = r * sin(a); // y = r times sin(and the A value)
 			let z = r * cos(angle); // z value = r value times cos(and angle value)
       
+      //let val = slider.value();
+  
     
       
 
